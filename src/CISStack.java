fa819898594531ac1987c33829c90753a4dd91d3
ChJus <92497757+ChJus@users.noreply.github.com>
2025-01-23 19:20:48 +0800

    finish

--- src/CISStack.java
+++ src/CISStack.java
@@ -1,24 +1,59 @@
 import java.util.Arrays;
 
 public class CISStack {
+  // Array property.
+  private Object[] array;
 
-    // Array property.
+  // Size property.
+  private int size = 0;
 
-    // Size property.
+  // Pointer property
+  private int pointer = -1;
 
-    // Pointer property
+  // Constructor.
+  public CISStack(int size) {
+    array = new Object[size];
+  }
 
-    // Constructor.
+  // Push. This method pushes a value onto the top of the stack.
+  public void push(Object element) {
+    pointer++;
+    if (pointer == array.length) {
+      Object[] temp = array;
+      array = Arrays.copyOf(temp, temp.length + 1);
+    }
+    size++;
+    array[pointer] = element;
+  }
 
-    // Push. This method pushes a value onto the top of the stack.
+  // Pop. This method pops a value from the top of the stack.
+  public Object pop() {
+    if (isEmpty()) return null;
+    size--;
+    return array[pointer--];
+  }
 
-    // Pop. This method pops a value from the top of the stack.
+  // isEmpty. Returns a boolean indicating whether the linked list is empty.
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-    // isEmpty. Returns a boolean indicating whether the linked list is empty.
+  // size. Returns the size of the queue.
+  public int size() {
+    return size;
+  }
 
-    // size. Returns the size of the queue.
+  // toString. Returns a description of the queue in, for example, the following format:
+  // CISStack{stack=[7, 11], size=2, pointer=1}
+  @Override
+  public String toString() {
+    String result = "CISStack{stack=[";
 
-    // toString. Returns a description of the queue in, for example, the following format:
-    // CISStack{stack=[7, 11], size=2, pointer=1}
+    for (int i = 0; i < array.length; i++) {
+      result += array[i] + (i == array.length - 1 ? "" : ", ");
+    }
 
+    result += "], size=" + size + ", pointer=" + pointer + "}";
+    return result;
+  }
 }
